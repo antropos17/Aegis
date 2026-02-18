6d2780872dd9060bd782571a033341db5f771ca6
﻿Antropos7 <antropos17@users.noreply.github.com>
2026-02-18 06:25:29 -0500

    chore: rename app.html to index.html, update vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     outDir: '../../dist/renderer',
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'src/renderer/app.html')
+      input: resolve(__dirname, 'src/renderer/index.html')
     }
   }
 });
